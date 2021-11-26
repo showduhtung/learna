b4bc19dd3132d7cc9b8c02b9d924b1b459d49a8e
Shaun Tung <shaun.tung@gmail.com>
2021-11-26 16:48:10 +0800

    bootstrapped and component/textfield

---
+++ packages/components/src/components/textfield/Textfield.jsx
@@ -0,0 +1,76 @@
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/system';
+import { forwardRef } from 'react';
+
+// const useStyles = makeStyles(() => ({
+//   root: (props) => ({
+//     pointerEvents: props.disabled && 'none',
+//   }),
+//   disabled: {
+//     backgroundColor: '#ECECEC',
+//   },
+// }));'
+
+const TextfieldStyles = {
+  // "& label.Mui-focused": {
+  //   color: "green",
+  // },
+  // "& .MuiInput-underline:after": {
+  //   borderBottomColor: "green",
+  // },
+  // "& .MuiOutlinedInput-root": {
+  //   "& fieldset": {
+  //     borderColor: "red",
+  //   },
+  //   "&:hover fieldset": {
+  //     borderColor: "yellow",
+  //   },
+  //   "&.Mui-focused fieldset": {
+  //     borderColor: "green",
+  //   },
+  // },
+  '&:disabled': {
+    pointerEvents: 'none',
+    backgroundColor: '#ECECEC',
+  },
+  backgroundColor: 'red',
+};
+
+export const Textfield = forwardRef(
+  (
+    {
+      InputProps,
+      onEnter = null,
+      value,
+      setValue = null,
+      variant = 'outlined',
+      suffix = null,
+      allowClear = true,
+      onChange = null,
+      disabled = false,
+      ...props
+    },
+    ref,
+  ) => {
+    const handleKeyPressEnter = (event) => {
+      if (event.key === 'Enter') {
+        onEnter && onEnter(event);
+      }
+    };
+
+    return styled(
+      <TextField
+        variant={variant}
+        fullWidth
+        inputRef={ref}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        onKeyPress={handleKeyPressEnter}
+        // InputProps={{ ...InputProps, classes: { disabled: classes.disabled } }}
+        // className={classes.root}
+        {...props}
+      />,
+    )(TextfieldStyles);
+  },
+);
---
+++ packages/components/src/config/aliases.js
@@ -0,0 +1,5 @@
+const aliases = (prefix = `src`) => ({
+  '@components': `${prefix}/components`,
+});
+
+module.exports = aliases;
